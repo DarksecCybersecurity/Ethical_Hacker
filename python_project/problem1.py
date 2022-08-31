f27707c1a2bf84d405ea418cabe1402af7f5e061
Darkseccybersecurity <darksec2022@gmail.com>
2022-08-31 03:53:22 -0500

    python projects

--- python_project/problem1.py
+++ python_project/problem1.py
@@ -9,7 +9,7 @@ class Calculation():
         print (f'                Calculo')
         print ('-' * 60)
         while True:
-            open = input("""Choois the option
+            open = input("""Choice the option
             \r 1.  Multiplication
             \r 2.  Division
             \r 3.  subtraction
@@ -18,26 +18,26 @@ class Calculation():
             \r Enter: """).upper()
 
             if open == '1':
-                n1 = input("Type number 1: ")
-                n2 = input("Type number 2: ")
+                n1 = input("Enter first number: ")
+                n2 = input("Enter second number: ")
                 result = int(float(n1)) * int(float(n2))
                 print(f"result {result}")
             
             elif open == '2':
-                n1 = input("Type number 1: ")
-                n2 = input("Type number 2: ")
+                n1 = input("Enter first number: ")
+                n2 = input("Enter second number: ")
                 result = int(float(n1)) / int(float(n2))
                 print(f"result {result}")
             
             elif open == '3':
-                n1 = input("Type number 1: ")
-                n2 = input("Type number 2: ")
+                n1 = input("Enter first number: ")
+                n2 = input("Enter second number: ")
                 result = int(float(n1)) - int(float(n2))
                 print(f"result {result}")
             
             elif open == '4':
-                n1 = input("Type number 1: ")
-                n2 = input("Type number 2: ")
+                n1 = input("Enter first number: ")
+                n2 = input("Enter second number: ")
                 result = int(float(n1)) + int(float(n2))
                 print(f"result {result}")
 
